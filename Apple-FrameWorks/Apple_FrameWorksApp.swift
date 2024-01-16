d4d86f7d2830a8f1edee1b91fabae1daad192c35
ShiaoC <yan095565@gmail.com>
2024-01-17 00:11:41 +0800

    Initial Commit

---
+++ Apple-FrameWorks/Apple_FrameWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorksApp.swift
+//  Apple-FrameWorks
+//
+//  Created by 蕭張媛 on 2024/1/17.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-FrameWorks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple-FrameWorks
+//
+//  Created by 蕭張媛 on 2024/1/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
